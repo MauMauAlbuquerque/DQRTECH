0a33418e47c7055cb3ed8816e1f6eb41b47f7b0e
MauMauAlbuquerque <86385175+MauMauAlbuquerque@users.noreply.github.com>
2023-08-15 09:46:03 -0300

    Update dbConfig.js

--- KnightsChallengeServer/src/config/dbConfig.js
+++ KnightsChallengeServer/src/config/dbConfig.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb+srv://knightschallenge:5258IQjdWS7ijeYS@cluster0.f5onqih.mongodb.net/knightschallenge')
+mongoose.connect('mongodb+srv://<cluster>:<password>@cluster0.f5onqih.mongodb.net/<database>')
 
 export default mongoose.connection
